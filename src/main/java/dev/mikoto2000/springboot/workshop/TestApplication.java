fa8ea69875ebac70cf2be395eea990bd007f902e
mikoto2000 <mikoto2000@gmail.com>
2025-09-14 03:06:54 +0000

    Added template project.

---
+++ src/main/java/dev/mikoto2000/springboot/workshop/TestApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.springboot.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mikoto2000/springboot/workshop/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.springboot.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
