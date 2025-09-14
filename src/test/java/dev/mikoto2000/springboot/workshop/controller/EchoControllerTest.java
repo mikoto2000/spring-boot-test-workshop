79580e17c90bf620d4b435c2f60558f3add45f9d
mikoto2000 <mikoto2000@gmail.com>
2025-09-14 03:52:43 +0000

    Added EchoControllerTest.

---
+++ src/test/java/dev/mikoto2000/springboot/workshop/controller/EchoControllerTest.java
@@ -0,0 +1,37 @@
+package dev.mikoto2000.springboot.workshop.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * EchoControllerTest
+ */
+@WebMvcTest(EchoController.class)
+public class EchoControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void testEcho() throws Exception {
+    mockMvc.perform(get("/echo").param("message", "12345"))
+      .andExpect(status().isOk())
+      .andExpect(content().string("12345"));
+  }
+
+  @BeforeEach
+  public void setup() {
+  }
+
+  @AfterEach
+  public void tearDown() {
+  }
+}
